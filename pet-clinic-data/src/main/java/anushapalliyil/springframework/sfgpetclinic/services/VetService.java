1e7279145d7c0c0a775bad94d16a8dcb1713612a
Anusha Palliyil <palliyil_anusha@bah.com>
2022-04-14 10:02:47 -0400

    refactoring to common crud interface. closes #11

--- pet-clinic-data/src/main/java/anushapalliyil/springframework/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/anushapalliyil/springframework/sfgpetclinic/services/VetService.java
@@ -1,15 +1,7 @@
 package anushapalliyil.springframework.sfgpetclinic.services;
 
-import java.util.Set;
-
 import anushapalliyil.springframework.sfgpetclinic.model.Vet;
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save (Vet vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
     
 }
